db50c60ad78ffccf3bedad775d09c8a0b3a6e9fe
evgenyslab <e.nuger@gmail.com>
2020-07-12 09:52:49 -0400

    checkpoint with new structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+
+extensions = [
+  Extension(
+    name="cppInterfacePackage",
+    sources=["interfacepackage/*.pyx"], # cython sources
+    include_dirs=["sources/include"],
+    libraries=["cppinterface"], # this is the c++ library name from CMakeLists.txt
+    library_dirs=["sources/bin"],
+    extra_compile_args=['-std=c++11'],
+    language="c++"
+    )
+
+]
+
+
+setup(
+    name = "pyCppInterface", # name of package in 'pip freeze'
+    # package_dir = {"":"build",
+    #                "":"python"}, # site_packages, having "" in this list
+    ext_modules = cythonize(extensions)
+)
